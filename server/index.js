93892f3090a2b9d54a7442dad8b88be337dd7a3c
Hridayash Pariyar <hridayashpariyar@Hridayashs-MacBook-Pro.local>
2024-10-20 20:28:45 -0400

    init

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+
+const app = express()
+app.use(cors())
+dotenv.config()
+
+
+
+app.get('/' , (req,res)=>{
+    res.send("hello world ")
+})
+
+
+app.listen(3002, ()=>{
+    console.log("server is running on port 3002")
+})
