28cabf3126be25839bf9a30d47871c2d1574379d
Hridayash Pariyar <hridayashpariyar@Hridayashs-MacBook-Pro.local>
2024-10-21 17:55:49 -0400

    added prisma schema and basic multipage form for frontend

---
+++ client/src/page/dashboard.tsx
@@ -0,0 +1,10 @@
+
+const Dashboard : React.FC = () =>{
+        return(
+            <h1>
+                This is home 
+            </h1>
+        );
+}
+
+export default Dashboard;
---
+++ client/src/page/signup.tsx
@@ -0,0 +1,9 @@
+const Signup : React.FC = ()=>{
+    return (
+     <h1>This is Signup page</h1>
+    );
+ }
+ 
+ 
+ 
+ export default Signup
