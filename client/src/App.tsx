d1cb4b358786e1482352be47143885ad8f4267c2
Samir Basnet <samirbasnet@samirs-mbp.home>
2024-10-21 22:02:13 -0400

    added login signup ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,23 @@
-import {Route, Routes, BrowserRouter} from "react-router-dom"
-import Dashboard from "./page/dashboard"
-import Login from "./page/login"
-import Signup from "./page/signup"
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Signup from "./page/Signup";
+import Header from "./page/Header";
+import Footer from "./page/Footer";
+import Login from "./page/login";
 
-const  App: React.FC =  ()=> {
 
-
+const App: React.FC = () => {
   return (
-   <>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/home" element ={<Dashboard />} />
-              <Route path="/login" element ={<Login/>} />
-              <Route path="/signup" element ={<Signup/>} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to BlendIn!</h1>} /> {/* Optional homepage */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/Header" element={<Header />} />
+        <Route path="/Footer" element={<Footer />} />
+        <Route path="/Login" element={<Login />} />
 
-            </Routes>
-          </BrowserRouter>
-   </>
-    
-    
-  )
-}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
