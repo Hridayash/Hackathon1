a6a3b7ead8b2213d2d7ce0db1eafa849cce77d24
Hridayash Pariyar <hridayashpariyar@Hridayashs-MacBook-Pro.local>
2024-10-23 18:46:18 -0400

    something

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/landingpage" element={<LandingPage />} />
+      <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
